0b3e1cc14c2682b3718f8fc4384d154da361d0f8
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2022-11-28 23:55:25 +0300

    Modified ProductRepositoryTest.java file

--- src/test/java/my/idea/list/ProductRepositoryTest.java
+++ src/test/java/my/idea/list/ProductRepositoryTest.java
@@ -1,5 +1,54 @@
 package my.idea.list;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class ProductRepositoryTest {
 
+    private ProductRepository repository = new ProductRepository();
+    Product book = new Book(1, "Александр использует XsMax", 1_000, "Александр Веденеев");
+    Product smartphone = new Smartphone(2, "XsMax", 35_000, "Apple");
+    Product product = new Product(3, "Чехол", 30);
+
+    @Test
+    public void shouldDeleteOneProductById() {
+        repository.addNewProduct(book);
+        repository.addNewProduct(smartphone);
+        repository.addNewProduct(product);
+        repository.deleteById(2);
+        Product[] expected = { book, product };
+        Product[] actual = repository.findAllProduct();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDeleteAllProductsById() {
+        repository.addNewProduct(book);
+        repository.addNewProduct(smartphone);
+        repository.addNewProduct(product);
+        repository.deleteById(1);
+        repository.deleteById(2);
+        repository.deleteById(3);
+        Product[] expected = {};
+        Product[] actual = repository.findAllProduct();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAddOneNewProduct() {
+        repository.addNewProduct(book);
+        Product[] expected = {book};
+        Product[] actual = repository.findAllProduct();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindAllProduct() {
+        repository.addNewProduct(book);
+        repository.addNewProduct(smartphone);
+        repository.addNewProduct(product);
+        Product[] expected = { book, smartphone, product};
+        Product[] actual = repository.findAllProduct();
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
