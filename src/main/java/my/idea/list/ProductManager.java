51628a50f2f81f140ce24d97a1b3d26676dcaf2b
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2022-11-28 23:52:59 +0300

    Modified ProductManager.java file

--- src/main/java/my/idea/list/ProductManager.java
+++ src/main/java/my/idea/list/ProductManager.java
@@ -2,4 +2,36 @@ package my.idea.list;
 
 public class ProductManager {
 
+    private ProductRepository repository;
+
+    public ProductManager(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public void add(Product product) {
+        repository.addNewProduct(product);
+    }
+
+    public boolean matches(Product product, String search) {
+        if (product.getName().contains(search)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Product[] searchBy(String text) {
+        Product[] result = new Product[0];
+        for (Product product : repository.findAllProduct()) {
+            if (matches(product, text)) {
+                int length = result.length + 1;
+                Product[] tmp = new Product[length];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                int lastIndex = tmp.length - 1;
+                tmp[lastIndex] = product;
+                result = tmp;
+            }
+        }
+        return result;
+    }
 }
